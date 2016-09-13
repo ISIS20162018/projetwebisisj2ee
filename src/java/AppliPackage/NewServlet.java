aec8b079e53b2bc903b05871fef421558f6c53ab
LeslieLeturgie <dumontier.leslie@hotmail.fr>
2016-09-13 11:48:36 +0200

    commit leslie

--- src/java/AppliPackage/NewServlet.java
+++ src/java/AppliPackage/NewServlet.java
@@ -40,7 +40,7 @@ public class NewServlet extends HttpServlet {
             out.println("<title>Servlet NewServlet</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet NewServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet NewServlet at " + request.getContextPath() + " coucou leslie</h1>");
             out.println("</body>");
             out.println("</html>");
         }
